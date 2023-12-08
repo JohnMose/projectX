a246405b7e1f42f3f90128b34463b740a128d7bb
JemmyKuria <kuriajemmy@gmail.com>
2023-12-08 20:17:30 +0300

    Changed header

--- projectxx/src/components/Header.js
+++ projectxx/src/components/Header.js
@@ -24,7 +24,7 @@ const Header = () => {
               Services
             </Button>
             <Button component={RouterLink} to="/contact" color="inherit">
-              Contact
+              Contact Us
             </Button>
           </Box>
         </Box>
